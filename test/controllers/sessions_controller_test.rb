c3bb7142adf0ce097e921352bd6c50150d043364
SergePauli <sergepauli@yandex.ru>
2022-09-04 15:52:26 +0700

    Создание тестов для sessions_controller

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "right route, and can't login with wrong password" do
+    post "/login",
+      params: { name: "User0", password: "test" }
+    assert_equal "create", @controller.action_name
+    assert_response :forbidden
+  end
+
+  test "can login" do
+    post "/login",
+      params: { name: users(:one).name, password: "pass0" }
+    assert_response :ok
+    assert_match "token", @response.body
+    assert_match JsonWebToken.generate_token(users(:one).id), @response.body
+  end
 end
