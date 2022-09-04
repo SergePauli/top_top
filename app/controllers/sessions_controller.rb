02c288a39a44af724bf1dc7f2581800a329c0b5d
SergePauli <sergepauli@yandex.ru>
2022-09-04 07:29:30 +0700

    валидация параметров запроса в sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,12 @@ class SessionsController < ApplicationController
   # POST Эндпоинт для получения токена авторизации
   # url - /login
   def create
-    user_params
+    ## валидируем входные параметры
+    render json: { message: "непролный набор данных" }, status: :bad_request if params[:name].blank? || params[:password].blank?
+
     ## Ищем пользователя, с указаным на входе именем
     @user = User.find_by(name: params[:name])
+
     ## Если пользователь найден, то проверяем пароль на входе, создаем и возвращаем токен
     if !!@user && @user.authenticate(params[:password])
       token = JsonWebToken.generate_token(@user.id)
@@ -13,11 +16,5 @@ class SessionsController < ApplicationController
       ## Иначе возвращаем ошибку авторизации
       render json: { error: "В доступе отказано" }, status: :forbidden
     end
-  end
-
-  private
-
-  def user_params
-    params.permit(:name, :password)
   end
 end
